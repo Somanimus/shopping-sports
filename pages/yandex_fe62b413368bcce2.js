60448ae9dd1c24138e741ac40207baec0cb3bdc6
Ruslan Sariev <rusisariev@gmail.com>
2022-06-28 15:40:29 +0600

    init

---
+++ pages/yandex_fe62b413368bcce2.js
@@ -0,0 +1,15 @@
+import Head from "next/head"
+
+
+const Yandex_fe62b413368bcce2 = () => {
+    return (
+        <div>
+            <Head>
+                <meta httpEquiv="Content-type" content="text/html; charset=UTF-8"/>
+            </Head>
+            Verification: fe62b413368bcce2
+        </div>
+    )
+}
+
+export default Yandex_fe62b413368bcce2
