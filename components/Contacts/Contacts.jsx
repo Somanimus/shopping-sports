d028ebffd5f4a979d2675ff867b435b34bffeebe
Somanimus <kavosm325@gmail.com>
2023-01-23 09:33:13 +0600

    fixed API and a few styles

--- components/Contacts/Contacts.jsx
+++ components/Contacts/Contacts.jsx
@@ -5,23 +5,16 @@ const Contacts = () => {
     return (
         <section className={"container "}>
             <div className={styles.contacts_left}>
-                <h1 className={styles.contacts_h1}>Контакты</h1>
-                <p className={styles.contacts_p}>
-                    Номер телефона: +(996)777 001713
-                </p>
-                <p className={styles.contacts_p}>
-                    Адрес компании: Кыргызская Республика г. Бишкек, ул.
-                    Токтоналиева 111
-                </p>
+                <h1 className={styles.contacts_h1}></h1>
+                <p className={styles.contacts_p}></p>
+                <p className={styles.contacts_p}></p>
             </div>
 
             <div className={styles.contacts_right}>
-                <h1 className={styles.contacts_h1}>Также связаться можно:</h1>
+                <h1 className={styles.contacts_h1}></h1>
                 <p className={styles.contacts_p}>
                     Почта:{" "}
-                    <a href="mailto:corpusmebelkg@gmail.com">
-                        corpusmebelkg@gmail.com
-                    </a>
+                    <a href=""></a>
                 </p>
             </div>
             <div className={styles.iframe}>
--- https.js
+++ https.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://212.42.103.101:8920/api"
+export const API_URL = "http://139.162.190.226:450/api"
 // export const API_URL = "http://137.184.232.116/api"
