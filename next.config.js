bea55286f641fe54a5f859122c7b7b96c360f221
Somanimus <kavosm325@gmail.com>
2023-01-31 22:43:32 +0600

    ESlint fixes

--- next.config.js
+++ next.config.js
@@ -2,9 +2,10 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
-        domains: ['137.184.232.116'],
+        domains: ['137.184.232.116', 'static.wixstatic.com'],
         unoptimized: true,
     },
+
 };
 
 module.exports = nextConfig;
--- pages/accepted_payments/index.jsx
+++ pages/accepted_payments/index.jsx
@@ -1,6 +1,7 @@
 ﻿import React from 'react';
 import Layout from '../../components/Layout/Layout';
 import styles from './acceptedPayments.module.css'
+import Image from 'next/image';
 
 const index = () => {
     return (
@@ -9,15 +10,15 @@ const index = () => {
                 <h1 className={styles.headline}>Принимаемые платежы:</h1>
                 <div className={styles.payment_wallets}>
                     <div className={styles.wallets}>
-                        <img src="https://static.wixstatic.com/media/e13a0d_ec131de30c3943e1849ad0a022281194~mv2.png/v1/fill/w_81,h_81,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/optima%20bank.png" alt="optima_icon" />
+                        <Image src="https://static.wixstatic.com/media/e13a0d_ec131de30c3943e1849ad0a022281194~mv2.png/v1/fill/w_81,h_81,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/optima%20bank.png" width={'100px'} height={'100px'} alt="optima_icon" />
                         <span>1. Optima Bank</span>
                     </div>
                     <div className={styles.wallets}>
-                        <img src="https://static.wixstatic.com/media/e13a0d_a96b408bc914464bbd0dbf1284f66ccb~mv2.png/v1/fill/w_81,h_81,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/unnamed_webp.png" alt="o_icon" />
+                        <Image src="https://static.wixstatic.com/media/e13a0d_a96b408bc914464bbd0dbf1284f66ccb~mv2.png/v1/fill/w_81,h_81,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/unnamed_webp.png" width={'100px'} height={'100px'} alt="o_icon" />
                         <span>2. О! Деньги</span>
                     </div>
                     <div className={styles.wallets}>
-                        <img src="https://static.wixstatic.com/media/e13a0d_85500d3a18a94b29b10d8c1bd4b3ee61~mv2.png/v1/fill/w_81,h_81,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/elsom_edited.png" alt="elsom_icon" />
+                        <Image src="https://static.wixstatic.com/media/e13a0d_85500d3a18a94b29b10d8c1bd4b3ee61~mv2.png/v1/fill/w_81,h_81,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/elsom_edited.png" width={'100px'} height={'100px'} alt="elsom_icon" />
                         <span>3. Элсом</span>
                     </div>
                 </div>
