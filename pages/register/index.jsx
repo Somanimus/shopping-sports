63042bc28b6ab80ff7b399e7683565cec1720e70
Somanimus <kavosm325@gmail.com>
2023-02-13 11:42:20 +0600

    register form done

---
+++ pages/register/index.jsx
@@ -0,0 +1,71 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import axios from "axios";
+import styles from './register.module.css'
+import React, { useState } from "react";
+import Layout from "../../components/Layout/Layout";
+import { API_URL } from "../../https";
+
+const index = () => {
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (data.password === data.confirmPassword) {
+        axios.post(`${API_URL}registration/`, {
+            email: data.email,
+            password: data.password
+        }).then(res => console.log(res)).catch(err => console.log(err))}
+        else{
+            alert('please fill all the fields correctly')
+        }
+
+  };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <Layout title="register">
+      <section className="container">
+        <form action="register" onSubmit={(e) => handleSubmit(e)} className={styles.form}>
+            <h1>Registration</h1>
+            <p>please enter your credentials to register</p>
+          <input
+          className={styles.input}
+            value={data.email}
+            type="email"
+            name="email"
+            placeholder="email"
+            onChange={(e) =>handleChange(e)}
+          />
+          <input
+          className={styles.input}
+            value={data.password}
+            type="password"
+            name="password"
+            placeholder="password"
+            onChange={e =>handleChange(e)}
+          />
+          <input
+          className={styles.input}
+            value={data.confirmPassword}
+            type="password"
+            placeholder="confirm password"
+            name="confirmPassword"
+            onChange={e =>handleChange(e)}
+          />
+          <button type="submit" className={styles.button}>Submit</button>
+        </form>
+      </section>
+    </Layout>
+  );
+};
+
+export default index;
